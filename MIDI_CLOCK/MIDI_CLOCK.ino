0c8b1b108b9add97d55c08566b5851764be969d0
joe beuckman <joebeuckman@gmail.com>
2016-02-11 20:44:09 -0600

    working and now with set clock divisor options

--- MIDI_CLOCK/MIDI_CLOCK.ino
+++ MIDI_CLOCK/MIDI_CLOCK.ino
@@ -1,17 +1,20 @@
 #include <MIDI.h>
 
 #define START_PIN 10
-#define CLOCK_PIN 9
 #define CONTINUE_PIN 11
 #define STOP_PIN 12
 
-
-#define DIVISOR_PIN A0
-int divisor = 1;
+#define CLOCK_PIN 9
 int clockCount = 0;
 
+#define DIVISOR_PIN A0
+int divisor = 24;
+int divisors[] = { 3, 4, 6, 8, 12, 16, 24, 36, 48, 72, 96 };  // possible parts of 24 ppqn 
+int divisorCount = sizeof(divisors) / sizeof(int);  // num elements in array
+int divisorRange = 1024 / divisorCount; // how to split the analog input
+
+#define TRIGGER_PULSE_MS 2
 int startTriggerRemaining = 0, clockTriggerRemaining = 0, continueTriggerRemaining = 0, stopTriggerRemaining = 0;
-#define TRIGGER_PULSE_MS 10
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
@@ -23,12 +26,14 @@ void handleStart() {
 }
 
 void handleClock() {
-  clockCount++;
-  if (clockCount == divisor) {
-    digitalWrite(CLOCK_PIN, HIGH);
-    clockTriggerRemaining = TRIGGER_PULSE_MS;
+  if (clockCount >= divisor) {
     clockCount = 0;
   }
+  if (clockCount == 0) {
+    digitalWrite(CLOCK_PIN, HIGH);
+    clockTriggerRemaining = TRIGGER_PULSE_MS;
+  }
+  clockCount++;
 }
 
 void handleContinue() {
@@ -38,48 +43,8 @@ void handleContinue() {
 
 void handleStop() {
   digitalWrite(STOP_PIN, HIGH);
+  digitalWrite(START_PIN, LOW);
   stopTriggerRemaining = TRIGGER_PULSE_MS;
-}
-
-
-
-void setup()
-{
-    
-    pinMode(START_PIN, OUTPUT);
-    digitalWrite(START_PIN, LOW);
-    pinMode(CLOCK_PIN, OUTPUT);
-    digitalWrite(CLOCK_PIN, LOW);
-    pinMode(CONTINUE_PIN, OUTPUT);
-    digitalWrite(CONTINUE_PIN, LOW);
-    pinMode(STOP_PIN, OUTPUT);
-    digitalWrite(STOP_PIN, LOW);
-
-    MIDI.setHandleStart(handleStart);
-    MIDI.setHandleClock(handleClock);
-    MIDI.setHandleContinue(handleContinue);
-    MIDI.setHandleStop(handleStop);
-
-
-    cli();//stop interrupts
-    
-    //set timer1 interrupt at 1kHz
-    TCCR1A = 0;// set entire TCCR1A register to 0
-    TCCR1B = 0;// same for TCCR1B
-    TCNT1  = 0;//initialize counter value to 0;
-    // set timer count for 1khz increments
-    OCR1A = 1999;// = (16*10^6) / (1000*8) - 1
-    // turn on CTC mode
-    TCCR1B |= (1 << WGM12);
-    // Set CS11 bit for 8 prescaler
-    TCCR1B |= (1 << CS11);   
-    // enable timer compare interrupt
-    TIMSK1 |= (1 << OCIE1A);
-    
-    sei();//allow interrupts
-
-    
-    MIDI.begin();
 }
 
 
@@ -117,13 +82,50 @@ ISR(TIMER1_COMPA_vect) {
 }
 
 
-int divisorRange = 1024 / 96;
+ 
+void setup()
+{
+
+    cli();//stop interrupts
+    
+    //set timer1 interrupt at 1kHz
+    TCCR1A = 0;// set entire TCCR1A register to 0
+    TCCR1B = 0;// same for TCCR1B
+    TCNT1  = 0;//initialize counter value to 0;
+    // set timer count for 1khz increments
+    OCR1A = 1999;// = (16*10^6) / (1000*8) - 1
+    // turn on CTC mode
+    TCCR1B |= (1 << WGM12);
+    // Set CS11 bit for 8 prescaler
+    TCCR1B |= (1 << CS11);   
+    // enable timer compare interrupt
+    TIMSK1 |= (1 << OCIE1A);
+    
+    sei();//allow interrupts
+
+    
+    pinMode(START_PIN, OUTPUT);
+    digitalWrite(START_PIN, LOW);
+    pinMode(CLOCK_PIN, OUTPUT);
+    digitalWrite(CLOCK_PIN, LOW);
+    pinMode(CONTINUE_PIN, OUTPUT);
+    digitalWrite(CONTINUE_PIN, LOW);
+    pinMode(STOP_PIN, OUTPUT);
+    digitalWrite(STOP_PIN, LOW);
+
+    MIDI.setHandleStart(handleStart);
+    MIDI.setHandleClock(handleClock);
+    MIDI.setHandleContinue(handleContinue);
+    MIDI.setHandleStop(handleStop);
+
+    MIDI.begin();
+}
 
 
 
 void loop()
 {
-//  divisor = 1 + analogRead(DIVISOR_PIN) / divisorRange;
+  divisor = divisors[int(analogRead(DIVISOR_PIN) / divisorRange)];
 
   MIDI.read();
 }
